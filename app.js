86c5e4c11dc0eb470d7a5f9a5c34340a30b2a1da
Kerem Zopcuk <keremzopcuk@Kerems-MacBook-Pro.local>
2021-07-22 21:07:25 +0300

    Add gameState and track with Github

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const initGame = (function () {
     player2: localStorage.getItem('player2Score') || 0,
   };
 
+  const player1Indicator = 'x';
+  const player2Indicator = 'o';
+
   document.getElementById('postNames').addEventListener('click', postNames);
 
   // Get player names and hide form
   function postNames() {
     getNames();
-    toggleElements();
+    hideElements();
   }
 
   function hideElements() {
@@ -24,28 +27,29 @@ const initGame = (function () {
 
   // store names on localStorage and print them on screen
   function getNames() {
-    // Store names on LS
     let player1Name = document.getElementById('player1Name');
     let player2Name = document.getElementById('player2Name');
-    localStorage.setItem('player1', JSON.stringify(player1Name.value));
-    localStorage.setItem('player2', JSON.stringify(player2Name.value));
     // Get names from LS
     let getPlayer1Name = localStorage.getItem('player1');
     let getPlayer2Name = localStorage.getItem('player2');
+    // Store names on LS
+    if (getPlayer1Name == '""' || getPlayer2Name == '""') {
+      localStorage.setItem('player1', JSON.stringify(player1Name.value));
+      localStorage.setItem('player2', JSON.stringify(player2Name.value));
+    }
     // Print names with scores
     document.getElementById('player1Score').textContent = `${JSON.parse(getPlayer1Name)}: ${scores.player1}`;
     document.getElementById('player2Score').textContent = `${JSON.parse(getPlayer2Name)}: ${scores.player2}`;
-
     return { getPlayer1Name, getPlayer2Name };
   }
-
-  // Hide form, if LS stores names
-  if (getNames().getPlayer1Name !== '""' && getNames().getPlayer2Name !== '""') hideElements();
 
   const players = {
     player1: getNames().player1Name,
     player2: getNames().player1Name,
   };
+
+  // Hide the form, if LS stores names
+  if (players.player1 !== '""' && players.player2 !== '""') hideElements();
 
   // Define Buttons
   const box = document.querySelectorAll('.box');
@@ -72,42 +76,56 @@ const initGame = (function () {
 
   // Let's Play!
   let gameState = ['', '', '', '', '', '', '', '', ''];
+  let playing = true;
 
-  function xOrO() {
-    let currentBoxId = this.getAttribute('data-id');
-    let currentBox = document.querySelector(`[data-id="${currentBoxId}"]`);
+  function xOrO(e) {
+    if (playing) {
+      let currBoxIndex = e.target.dataset.id;
+      let currBox = e.target;
+      //? That might be a problem.
+      let countX = (countY = 0);
 
-    if (moves.length < 9) {
-      if (lastMove() === 'x') {
-        console.log('o');
-        moves.push('o');
-        currentBox.textContent = 'o';
-        console.log(`Box Number is ${currentBoxId}`);
-      } else if (lastMove() === 'o') {
-        console.log('x');
-        moves.push('x');
-        currentBox.textContent = 'x';
-        console.log(`Box Number is ${currentBoxId}`);
-      } else {
-        console.log('x');
-        moves.push('x');
-        currentBox.textContent = 'x';
-        console.log(`Box Number is ${currentBoxId}`);
+      // Track counts of X and Y
+      for (el of gameState) {
+        if (el == 'x') countX++;
+        if (el == 'o') countY++;
       }
+
+      // Chnage current value based on moves count. Note: First move is always X and then O. Everytime X == O means next turn is X.
+      let currValue;
+      if (countX == countY) {
+        currValue = player1Indicator;
+      } else if (countX < countY) {
+        currValue = player1Indicator;
+      } else if (countX > countY) {
+        currValue = player2Indicator;
+      }
+
+      // Push current value to gameState and screen.
+      if (gameState[currBoxIndex] == '') {
+        function pushGameState(val, curr) {
+          gameState[curr] = val;
+          currBox.textContent = val;
+        }
+        pushGameState(currValue, currBoxIndex);
+        console.log(gameState, currBoxIndex);
+      }
+      checkWinner();
     }
-    checkWinner();
   }
 
   function checkWinner() {}
-  // Reset game areo
+
+  // Reset game table
   function resetGame() {
     box.forEach((item) => (item.textContent = ''));
-    moves = [];
+    gameState = ['', '', '', '', '', '', '', '', ''];
   }
 
   return {
     getNames,
     xOrO,
     hideElements,
+    gameState,
   };
 })();
