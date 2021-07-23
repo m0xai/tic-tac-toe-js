7da1b5345bc3a2b947f6b39b8afd01584bbc7fc8
Kerem Zopcuk <keremzopcuk@Kerems-MacBook-Pro.local>
2021-07-23 23:04:38 +0300

    Add localStorage

--- app.js
+++ app.js
@@ -1,66 +1,88 @@
 // TODO: Add name change function
 
 const initGame = (function () {
-  const scores = {
-    player1: localStorage.getItem('player1Score') || 0,
-    player2: localStorage.getItem('player2Score') || 0,
-  };
-
   const player1Indicator = 'x';
   const player2Indicator = 'o';
 
   document.getElementById('postNames').addEventListener('click', postNames);
 
+  const players = {
+    player1: { name: 'Player 1', score: 0 },
+    player2: { name: 'Player 2', score: 0 },
+  };
+
   // Get player names and hide form
   function postNames() {
-    getNames();
+    grabNames();
     hideElements();
   }
 
   function hideElements() {
-    localStorage.setItem('hideItem', 'none');
-    localStorage.setItem('showItem', 'inline');
-    document.getElementById('playerNames').style.display = localStorage.getItem('hideItem');
-    document.getElementById('showPlayerNames').style.display = localStorage.getItem('showItem');
-    document.getElementById('showPlayerScores').style.display = localStorage.getItem('showItem');
+    document.getElementById('playerNames').style.display = 'none';
+    document.getElementById('scoreboard').style.display = 'inline';
   }
 
-  // store names on localStorage and print them on screen
-  function getNames() {
-    let player1Name = document.getElementById('player1Name');
-    let player2Name = document.getElementById('player2Name');
-    // Get names from LS
-    let getPlayer1Name = localStorage.getItem('player1');
-    let getPlayer2Name = localStorage.getItem('player2');
-    // Store names on LS
-    if (getPlayer1Name == '""' || getPlayer2Name == '""') {
-      localStorage.setItem('player1', JSON.stringify(player1Name.value));
-      localStorage.setItem('player2', JSON.stringify(player2Name.value));
-    }
-    // Print names with scores
-    document.getElementById('player1Score').textContent = `${JSON.parse(getPlayer1Name)}: ${scores.player1}`;
-    document.getElementById('player2Score').textContent = `${JSON.parse(getPlayer2Name)}: ${scores.player2}`;
-    return { getPlayer1Name, getPlayer2Name };
+  function grabNames() {
+    players.player1.name = document.getElementById('player1Name').value;
+    players.player2.name = document.getElementById('player2Name').value;
+    setLS('players', players);
   }
 
-  const players = {
-    player1: getNames().player1Name,
-    player2: getNames().player1Name,
-  };
+  function setLS(key, obj) {
+    localStorage.setItem(key, JSON.stringify(obj));
+  }
+
+  function getFromLS(obj) {
+    return JSON.parse(localStorage.getItem(obj));
+  }
+
+  function updateScoreLS(key, property, subProperty, value) {
+    let obj = getFromLS(key);
+    obj[property][subProperty] = value === 0 ? 0 : obj[property][subProperty] + value;
+    setLS(key, obj);
+  }
+
+  function printInfo() {
+    document.getElementById('showPlayer1').textContent = `${getFromLS('players').player1.name} : ${getFromLS('players').player1.score}`;
+    document.getElementById('showPlayer2').textContent = `${getFromLS('players').player2.name} : ${getFromLS('players').player2.score}`;
+  }
+  printInfo();
+
+  // Checks localStorega and if empty, then greabNames, but ifa has a value, than hide userForm
+  if (localStorage.length !== 0) {
+    hideElements();
+  } else {
+    grabNames();
+  }
+  // // store names on localStorage and print them on screen
+  // function getNames() {
+  //   let player1Name = document.getElementById('player1Name');
+  //   let player2Name = document.getElementById('player2Name');
+  //   // Get names from LS
+  //   let getPlayer1Name = localStorage.getItem('player1');
+  //   let getPlayer2Name = localStorage.getItem('player2');
+  //   // Store names on LS
+  //   if (getPlayer1Name == '""' || getPlayer2Name == '""') {
+  //     localStorage.setItem('player1', JSON.stringify(player1Name.value));
+  //     localStorage.setItem('player2', JSON.stringify(player2Name.value));
+  //   }
+  //   // Print names with scores
+  //   document.getElementById('player1Score').textContent = `${JSON.parse(getPlayer1Name)}: ${getPlayers.player1.score}`;
+  //   document.getElementById('player2Score').textContent = `${JSON.parse(getPlayer2Name)}: ${getPlayers.player2.score}`;
+  //   return { getPlayer1Name, getPlayer2Name };
+  // }
+
+  // const players = {
+  //   player1: getNames().player1Name,
+  //   player2: getNames().player1Name,
+  // };
 
   // Hide the form, if LS stores names
-  if (players.player1 !== '""' && players.player2 !== '""') hideElements();
 
   // Define Buttons
   const box = document.querySelectorAll('.box');
   const resetBtn = document.getElementById('resetBtn');
   resetBtn.addEventListener('click', resetGame);
-
-  // Init Moves
-  let moves = [];
-  let lastMove = function () {
-    return moves[moves.length - 1];
-  };
 
   // Add functionalitys to boxes
   box.forEach((item) => {
@@ -126,15 +148,20 @@ const initGame = (function () {
       [2, 4, 6],
     ];
 
+    // Pair every conditions with a variable.
     for (let i = 0; i <= 7; i++) {
       let a = winningConditions[i][0];
       let b = winningConditions[i][1];
       let c = winningConditions[i][2];
 
+      // Compare conditions with current game state.
       if (gameState[a] == '' || gameState[b] == '' || gameState[c] == '') {
         continue;
       } else if (gameState[a] == gameState[b] && gameState[a] == gameState[c] && gameState[b] == gameState[c]) {
-        console.log(gameState[a] + 'Wins!');
+        console.log(gameState[a].toUpperCase() + ' Wins!');
+        if (gameState[a] == 'x') updateScoreLS('players', 'player1', 'score', 1);
+        if (gameState[a] == 'x') getPlayers.player2.score++;
+        updateScreen();
         playing = false;
       }
     }
@@ -144,12 +171,16 @@ const initGame = (function () {
   function resetGame() {
     box.forEach((item) => (item.textContent = ''));
     gameState = ['', '', '', '', '', '', '', '', ''];
+    updateScoreLS('players', 'player1', 'score', 0);
+    updateScoreLS('players', 'player2', 'score', 0);
+    printInfo();
   }
 
   return {
-    getNames,
     xOrO,
-    hideElements,
     gameState,
+    setLS,
+    getFromLS,
+    updateScoreLS,
   };
 })();
